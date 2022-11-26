4d18be5ba5299474ba8b42019bda8687c2043bb2
Vetrichelvan97 <113371553+Vetrichelvan97@users.noreply.github.com>
2022-11-26 21:22:38 +0530

    Add files via upload

---
+++ Finale Deliverables/Working Code/src/pages/components-overview/compareLocations/index.js
@@ -0,0 +1,244 @@
+// project import
+import AuthWrapper from './PredictWrapper';
+import ComponentSkeleton from '../ComponentSkeleton';
+
+// ==============================|| COMPONENTS - PREDCTING MANUALLY||============================== //
+
+import React, { useState } from 'react';
+
+// material-ui
+import { Button, Grid, InputLabel, OutlinedInput, Stack, Typography } from '@mui/material';
+
+// third party
+import { Formik } from 'formik';
+import { toast } from 'react-toastify';
+import AnimateButton from 'components/@extended/AnimateButton';
+import options from 'menu-items/toastOptions';
+import logo from 'assets/images/icons/turbinehut- 70.svg';
+
+// ============================|| FIREBASE - LOGIN ||============================ //
+const PredictManual = () => {
+    const [latitude, setlatitude] = useState('');
+    const [longitude, setlongitude] = useState('');
+    const [latitude2, setlatitude2] = useState('');
+    const [longitude2, setlongitude2] = useState('');
+    const [isOutput, setisOutput] = useState(false);
+    const [Output, setOutput] = useState('');
+    const [Output2, setOutput2] = useState('');
+
+    const Predict = async () =>
+        await toast.promise(
+            fetch('http://127.0.0.1:5000/predict-lat-and-lng', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    apikey: '9d06a0a080e7ce38d35803bca42e0e1c',
+                    lat: latitude,
+                    lng: longitude
+                })
+            })
+                .then((res) => res.json())
+                .then((json) => {
+                    setOutput(Math.abs(json.result));
+                }),
+            fetch('http://127.0.0.1:5000/predict-lat-and-lng', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    apikey: '9d06a0a080e7ce38d35803bca42e0e1c',
+                    lat: latitude2,
+                    lng: longitude2
+                })
+            })
+                .then((res) => res.json())
+                .then((json) => {
+                    setOutput2(Math.abs(json.result));
+                    setisOutput(true);
+                }),
+            {
+                pending: 'Predicting from the server',
+                success: 'Prediction completed 👌',
+                error: 'Prediction rejected 🤯'
+            },
+            options
+        );
+    const handleSubmit = (e) => {
+        Predict();
+        e.preventDefault();
+    };
+    const handleBack = (e) => {
+        setisOutput(false);
+        setlongitude('');
+        setlatitude('');
+        e.preventDefault();
+    };
+    return (
+        <>
+            {!isOutput && (
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                        submit: null
+                    }}
+                >
+                    <form noValidate onSubmit={handleSubmit}>
+                        <Grid container spacing={2}>
+                            <h3 style={{ margin: '0 0 0 17px' }}>Location 1</h3>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="latitute">Latitude</InputLabel>
+                                    <OutlinedInput
+                                        id="latitute"
+                                        type="string"
+                                        value={latitude}
+                                        name="email"
+                                        onChange={(e) => {
+                                            setlatitude(e.target.value);
+                                        }}
+                                        placeholder="Enter Latitute"
+                                        fullWidth
+                                    />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="longitude">Longitude</InputLabel>
+                                    <OutlinedInput
+                                        id="longitude"
+                                        type="string"
+                                        value={longitude}
+                                        name="wind direction"
+                                        onChange={(e) => {
+                                            setlongitude(e.target.value);
+                                        }}
+                                        placeholder={'Enter Longitude'}
+                                    />
+                                </Stack>
+                            </Grid>
+                            <h3 style={{ margin: '20px 0 0 17px' }}>Location 2</h3>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="latitute">Latitude</InputLabel>
+                                    <OutlinedInput
+                                        id="latitute"
+                                        type="string"
+                                        value={latitude2}
+                                        name="email"
+                                        onChange={(e) => {
+                                            setlatitude2(e.target.value);
+                                        }}
+                                        placeholder="Enter Latitute"
+                                        fullWidth
+                                    />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="longitude">Longitude</InputLabel>
+                                    <OutlinedInput
+                                        id="longitude"
+                                        type="string"
+                                        value={longitude2}
+                                        name="wind direction"
+                                        onChange={(e) => {
+                                            setlongitude2(e.target.value);
+                                        }}
+                                        placeholder={'Enter Longitude'}
+                                    />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12} alignSelf="center">
+                                <AnimateButton>
+                                    <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="primary">
+                                        Predict
+                                    </Button>
+                                </AnimateButton>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Formik>
+            )}
+            {isOutput && (
+                <Formik>
+                    <form noValidate onSubmit={handleBack}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>The Wind Power predicted for</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>Latitude - {latitude}</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>Longitude - {longitude}</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                <Stack spacing={0}>
+                                    <InputLabel style={{ fontWeight: '700' }}>Wind Power - {Output}</InputLabel>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                        <br></br>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>The Wind Power predicted for</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>Latitude - {latitude2}</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={0}>
+                                    <InputLabel>Longitude - {longitude2}</InputLabel>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12} style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'space-between' }}>
+                                <Stack spacing={0}>
+                                    <InputLabel style={{ fontWeight: '700' }}>Wind Power - {Output2}</InputLabel>
+                                </Stack>
+                                <Stack spacing={0}>
+                                    <img src={logo} alt="Turbine Hut" srcset="" width="118" />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12} alignSelf="center">
+                                <AnimateButton>
+                                    <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="primary">
+                                        Go Back
+                                    </Button>
+                                </AnimateButton>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Formik>
+            )}
+        </>
+    );
+};
+
+const ComponentTypography = () => (
+    <ComponentSkeleton>
+        <AuthWrapper>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+                        <Typography variant="h3">Power Prediction</Typography>
+                    </Stack>
+                </Grid>
+                <Grid item xs={12}>
+                    <PredictManual />
+                </Grid>
+            </Grid>
+        </AuthWrapper>
+    </ComponentSkeleton>
+);
+
+export default ComponentTypography;
